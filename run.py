7dedfc248d312e585b221883a84374d8ad75204e
Nom115 <Noumair.rafiq@gmail.com>
2022-06-09 20:05:09 +0100

    Add board class with board printing

--- run.py
+++ run.py
@@ -1,7 +1,19 @@
 from random import randint
 
+class Board:
+    def __init__(self, row, col):
+        self.col = col
+        self.row = row
+
+    def print_board(self):
+        board = [["~" for a in range(self.col)] for b in range(self.row)]
+        for i in board:
+            print(" ".join(i))
+        
+
+board_a = Board(14, 14)
+board_b = Board(18, 18)
+board_c = Board(22, 22)
 
 
-board = [["~" for a in range(3)] for b in range(3)]
-for i in board:
-    print(" ".join(i))
+print(board_c.print_board())
