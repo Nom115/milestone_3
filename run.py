987cac9dc6215a4c81ebd90a353aacfd949e5e41
Nom115 <Noumair.rafiq@gmail.com>
2022-06-09 17:18:18 +0100

    Add initial game board logic

--- run.py
+++ run.py
@@ -1,3 +1,7 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+from random import randint
+
+
+
+board = [["~" for a in range(3)] for b in range(3)]
+for i in board:
+    print(" ".join(i))
