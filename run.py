ed002e7b9857e7a3e33730148685663b9a305259
Nom115 <Noumair.rafiq@gmail.com>
2022-06-09 20:40:43 +0100

    Add battleship class with ship A and B inheritance

--- run.py
+++ run.py
@@ -1,12 +1,13 @@
 from random import randint
 
+
 class Board:
     def __init__(self, row, col):
         self.col = col
         self.row = row
 
     def print_board(self):
-        board = [["~" for a in range(self.col)] for b in range(self.row)]
+        board = [["#" for a in range(self.col)] for b in range(self.row)]
         for i in board:
             print(" ".join(i))
         
@@ -16,4 +17,28 @@ board_b = Board(18, 18)
 board_c = Board(22, 22)
 
 
-print(board_c.print_board())
+class Battleship:
+    def __init__(self):
+        pass
+
+    def render(self):
+        pass
+
+class shipA(Battleship):
+    def __init__(self):
+        super(Battleship, self).__init__()
+        pass
+
+    def render(self):
+        print("Aaaaaa")
+
+class shipB(Battleship):
+    def __init__(self):
+        super(Battleship, self).__init__()
+        pass
+
+    def render(self):
+        print("Bbbbb")
+
+if __name__ == '__main__':
+    print(board_c.print_board())
