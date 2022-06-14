d3e1043be8ba1632ef0b865ce032bd93491d65f5
Nom115 <Noumair.rafiq@gmail.com>
2022-06-14 01:00:11 +0100

    Added jupyter notebook

--- run.py
+++ run.py
@@ -11,7 +11,13 @@ class Game:
     
     def random_number(self):
         return randint(0, len(self.board)-1)
-        
+
+    def random_direction(self):
+        direction = randint(0,1)
+        if direction == 0:
+            return True
+        else:
+            return False
 
 board_a = Game(14, 14)
 
@@ -19,16 +25,30 @@ board_a = Game(14, 14)
 
 class Battleship:
     def __init__(self):
-        self.length = None
+        self.length = 3
        
     def collision_check(self, game : Game):
-        random_place = game.random_number()
-        print(random_place)
-        row = game.board[random_place]
+        random_col = game.random_number()
+        random_row = game.random_number()
+        direction = game.random_direction()
+        print(random_col)
+        print(random_row)
+        row = game.board[random_row]
         print(row)
-        column = row[random_place]
+        column = row[random_col]
         print(column)
+        positions = []
+        for i in range(random_col, self.length):
+            temp_pos = 0
+            positions.append(column)
+            temp_pos +=1
+            print(row[random_col+temp_pos])
         
+        if len(positions) != self.length:
+            return "False"
+        print(positions)
+
+
 
     def render(self, game : Game):
         
