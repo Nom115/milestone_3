329b873fef53001519a8e29fcb56c38a37d43382
Nom115 <Noumair.rafiq@gmail.com>
2022-07-05 15:55:08 +0100

    Add logic to get user input

--- run.py
+++ run.py
@@ -39,6 +39,22 @@ class Battleship:
             self.board[self.x_row][self.y_column] = "X"
         return self.board
 
+    def get_user_input(self):
+        try:
+            x_row = input("Enter the row of the ship: ")
+            while x_row not in "12345678":
+                print("Not an appropriate choice, please select a valid row")
+                x_row = input("Enter the row of the ship: ")
+
+            y_column = input("Enter the column letter of the ship: ").upper()
+            while y_column not in "ABCDEFGH":
+                print("Not an appropriate choice, please select a valid column")
+                y_column = input("Enter the column letter of the ship: ").upper()
+            return int(x_row) - 1, GameBoard.get_letters_to_numbers()[y_column]
+        except ValueError and KeyError:
+            print("Not a valid input")
+            return self.get_user_input()
+
 
 if __name__ == "__main__":
     RunGame()
