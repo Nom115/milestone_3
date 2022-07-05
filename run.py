6e5bc786f6d5310280710ce805d5d27ebaca69c4
Nom115 <Noumair.rafiq@gmail.com>
2022-07-05 15:53:30 +0100

    Add Battleship class with create ship logic

--- run.py
+++ run.py
@@ -27,5 +27,18 @@ class GameBoard:
             row_number += 1
 
 
+class Battleship:
+    def __init__(self, board):
+        self.board = board
+
+    def create_ships(self):
+        for i in range(5):
+            self.x_row, self.y_column = random.randint(0, 7), random.randint(0, 7)
+            while self.board[self.x_row][self.y_column] == "X":
+                self.x_row, self.y_column = random.randint(0, 7), random.randint(0, 7)
+            self.board[self.x_row][self.y_column] = "X"
+        return self.board
+
+
 if __name__ == "__main__":
     RunGame()
