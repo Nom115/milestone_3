0a4f026689d23b2c25d9e030673f5b849bf81382
Nom115 <Noumair.rafiq@gmail.com>
2022-07-14 20:53:18 +0100

    Add Castle option to player directions

--- run.py
+++ run.py
@@ -38,6 +38,13 @@ def village():
         "To the East there is a sign, with many warning written in blood. The sign says, 'BEWARE\n The Dungeon of Neverending Torment\n This way'"
     )
     time.sleep(1)
+    print(
+        "The castle is not too far from here, if you wanted to, you could head there now."
+    )
+    time.sleep(1)
+    player_direction = input(
+        "Where would you like to go? \n A) The armoury \n B) The tavern \n C) The dungeon \n D) The Castle"
+    )
 
 
 # Begin the adventure
@@ -52,9 +59,9 @@ def RunGame():
     print(player.fur)
     time.sleep(1)
     print(
-        f"You are a prince, with {player.fur} fur, whose castle who has been taken over by the most evil, powerful cat-zard, Cattledoore. Now, Cattledoore is in possesion of all the catnip in Catnipdom, so you must go on a quest, to retake your kingdom and help your people, by finding the 'purrfect whisker of eternal nightmares and damnation jr the third'."
+        f"You are a prince, with {player.fur} fur, whose castle who has been taken over by the most evil, powerful cat-zard, Cattledoore. \n Now, Cattledoore is in possesion of all the catnip in Catnipdom, so you must go on a quest, to retake your kingdom and help your people, by finding the 'purrfect whisker of eternal nightmares and damnation jr the third'."
     )
-    time.sleep(1)
+    time.sleep(10)
     village()
     while player.health > 0:
         print(player.health)
