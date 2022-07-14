0de7e3717e2c80b4fe56170ad96610d15875394e
Nom115 <noumair.rafiq@gmail.com>
2022-07-14 15:05:23 +0100

    Add stamina to player

--- run.py
+++ run.py
@@ -1,12 +1,14 @@
 import random
 
+from grpc import StreamUnaryMultiCallable
+
 class Player:
-    def __init__(self, health):
+    def __init__(self, health, stamina):
         self.health = health
-        
+        self.stamina = stamina
 
 def RunGame():
-    player = Player(100)
+    player = Player(100, 10)
 
     print(player.health)
 
