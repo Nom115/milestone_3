59aab60bedd270b554e0aa474b62c65c224f9921
Nom115 <noumair.rafiq@gmail.com>
2022-07-14 15:36:44 +0100

    Add player fur customisation

--- run.py
+++ run.py
@@ -7,11 +7,22 @@ class Player:
         self.stamina = stamina
         self.inventory = inventory
 
+
+def get_player_fur():
+    get_player_fur = input("What colour is your fur? A)Brown, B) White, C)Black").upper()
+    while get_player_fur not in 'ABC':
+        print("Not a valid choice, please select a correct option")
+        get_player_fur = input("What colour is your fur? A)Brown, B) White, C)Black").upper()
+    return get_player_fur
+
+
 def RunGame():
     player = Player(100, 10, ["Sword of Destruction",])
     print("You are a prince, whose castle who has been taken over by the most evil, powerful cat-zard, Cattledoore. Now, Cattledoore is in possesion of all the catnip in Catnipdom, so you must go on a quest, to retake your kingdom and help your people, by finding the 'purrfect whisker of eternal nightmares and damnation jr the third'.")
+    player_fur = get_player_fur()
     
-    
+
+        
     while player.health > 0:
         print(player.health)
         
