764faa7287baba0ee355fd86b0528716dc8834ae
Nom115 <Noumair.rafiq@gmail.com>
2022-07-14 20:18:43 +0100

    Change player fur to be part of player class

--- run.py
+++ run.py
@@ -2,27 +2,27 @@ import random
 
 
 class Player:
-    def __init__(self, health, stamina, inventory):
+    def __init__(self, health, stamina, inventory, fur):
         self.health = health
         self.stamina = stamina
         self.inventory = inventory
+        self.fur = fur
 
-
-def get_player_fur():
-    p_fur = {
-        "A": "Brown",
-        "B": "White",
-        "C": "Black",
-    }
-    get_player_fur = input(
-        "What colour is your fur? A)Brown, B) White, C)Black"
-    ).upper()
-    while get_player_fur not in "ABC":
-        print("Not a valid choice, please select a correct option")
+    def get_player_fur(self):
+        p_fur = {
+            "A": "Brown",
+            "B": "White",
+            "C": "Black",
+        }
         get_player_fur = input(
             "What colour is your fur? A)Brown, B) White, C)Black"
         ).upper()
-    return p_fur[get_player_fur]
+        while get_player_fur not in "ABC":
+            print("Not a valid choice, please select a correct option")
+            get_player_fur = input(
+                "What colour is your fur? A)Brown, B) White, C)Black"
+            ).upper()
+        self.fur = p_fur[get_player_fur]
 
 
 def RunGame():
@@ -32,9 +32,10 @@ def RunGame():
         [
             "Sword of Destruction",
         ],
+        "Placeholder",
     )
-    player_fur = get_player_fur()
-    print(player_fur)
+    player.get_player_fur()
+    print(player.fur)
     print(
         "You are a prince, whose castle who has been taken over by the most evil, powerful cat-zard, Cattledoore. Now, Cattledoore is in possesion of all the catnip in Catnipdom, so you must go on a quest, to retake your kingdom and help your people, by finding the 'purrfect whisker of eternal nightmares and damnation jr the third'."
     )
