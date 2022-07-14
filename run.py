07327878c72b27a9dde675a6c05b2db784c0a9f6
Nom115 <Noumair.rafiq@gmail.com>
2022-07-14 20:46:38 +0100

    Add main village story

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 import random
+import time
 
 # Player Class
 class Player:
@@ -16,12 +17,12 @@ class Player:
             "C": "Black",
         }
         get_player_fur = input(
-            "What colour is your fur? A)Brown, B) White, C)Black"
+            "What colour is your fur? A)Brown, B) White, C)Black \n"
         ).upper()
         while get_player_fur not in "ABC":
             print("Not a valid choice, please select a correct option")
             get_player_fur = input(
-                "What colour is your fur? A)Brown, B) White, C)Black"
+                "What colour is your fur? A)Brown, B) White, C)Black \n"
             ).upper()
         self.fur = p_fur[get_player_fur]
 
@@ -31,16 +32,26 @@ def RunGame():
     player = Player(
         100,
         10,
-        [
-            "Sword of Destruction",
-        ],
+        [],
         "Placeholder",
     )
     player.get_player_fur()
     print(player.fur)
+    time.sleep(1)
     print(
-        "You are a prince, whose castle who has been taken over by the most evil, powerful cat-zard, Cattledoore. Now, Cattledoore is in possesion of all the catnip in Catnipdom, so you must go on a quest, to retake your kingdom and help your people, by finding the 'purrfect whisker of eternal nightmares and damnation jr the third'."
+        f"You are a prince, with {player.fur} fur, whose castle who has been taken over by the most evil, powerful cat-zard, Cattledoore. Now, Cattledoore is in possesion of all the catnip in Catnipdom, so you must go on a quest, to retake your kingdom and help your people, by finding the 'purrfect whisker of eternal nightmares and damnation jr the third'."
     )
+    time.sleep(1)
+    print("You walk into a village close to the castle in the dead of night.")
+    time.sleep(1)
+    print("To the West there is a shop called 'Yakobs Armoury'.")
+    time.sleep(1)
+    print("To the North there is a tavern with many revellers and patrons.")
+    time.sleep(1)
+    print(
+        "To the East there is a sign, with many warning written in blood. The sign says, 'BEWARE\n The Dungeon of Neverending Torment\n This way'"
+    )
+    time.sleep(1)
 
     while player.health > 0:
         print(player.health)
