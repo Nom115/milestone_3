195286377f9bb9ee17ee7b33bc052297bfa80878
Nom115 <noumair.rafiq@gmail.com>
2022-07-14 15:03:00 +0100

    Added player class with health

--- run.py
+++ run.py
@@ -1,5 +1,14 @@
 import random
 
+class Player:
+    def __init__(self, health):
+        self.health = health
+        
+
+def RunGame():
+    player = Player(100)
+
+    print(player.health)
 
 
 if __name__ == "__main__":
