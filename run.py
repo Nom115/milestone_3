a9d3b81993d464e9afd941a69beb09d0dc273ba0
Nom115 <noumair.rafiq@gmail.com>
2022-07-14 15:07:59 +0100

    Add inventory

--- run.py
+++ run.py
@@ -1,16 +1,17 @@
 import random
 
-from grpc import StreamUnaryMultiCallable
 
 class Player:
-    def __init__(self, health, stamina):
+    def __init__(self, health, stamina, inventory):
         self.health = health
         self.stamina = stamina
+        self.inventory = inventory
 
 def RunGame():
-    player = Player(100, 10)
+    player = Player(100, 10, ["Sword of Destruction",])
 
     print(player.health)
+    print(player.inventory)
 
 
 if __name__ == "__main__":
